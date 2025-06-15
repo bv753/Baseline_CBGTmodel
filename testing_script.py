df14ec1350f2951ae9d38c354714a83445ba3ae7
Benjamin Vogt <bvogt7503@gmail.com>
2025-06-15 12:06:28 +0200

    Calculate testing loss

--- testing_script.py
+++ testing_script.py
@@ -1,11 +1,17 @@
+import config_script as cs
 import model_functions as mf
 import plotting_functions as pf
 import training_script as trs
 
+import jax.numpy as jnp
+
 all_ys, all_xs, all_zs = mf.test_model(trs.params_nm)
 
-# TODO: calculate loss after testing
-# use mf.batched_nm_rnn_loss
+mean_ys = jnp.mean(all_ys, 0)  # mean over random seed iterations
+tst_inputs, tst_outputs, tst_masks = mf.self_timed_movement_task(cs.test_start_t, cs.config['T_cue'], cs.config['T_wait'], cs.config['T_movement'], cs.config['T'])
+
+testing_loss = jnp.sum(((mean_ys - tst_outputs) ** 2) * tst_masks) / jnp.sum(tst_masks)
+print("Testing loss:", testing_loss)
 
 pf.plot_output(all_ys)
 pf.plot_activity_by_area(all_xs, all_zs)
